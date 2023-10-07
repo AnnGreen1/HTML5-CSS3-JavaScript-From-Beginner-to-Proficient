47d4d49998be1688a9c911523e805aee5d25fcef
anngreen <anngreen@qq.com>
2023-10-07 22:46:56 +0800

    15、16、17、18的部分

---
+++ 15/15.5/index.js
@@ -0,0 +1,34 @@
+let obj = { name: 'yuanmingyue' }
+console.log(obj.toString());
+
+
+let num = 9;
+undefined
+
+console.log(num.toString());
+
+
+
+console.log(typeof num.toString());
+
+let a = 10
+undefined
+
+console.log(a.toString('8'));
+
+console.log(12.12.toString());
+
+num = '0123.45'
+console.log(parseInt(num));
+
+num = 010
+console.log(parseInt(num));
+
+console.log(parseInt(17,8));
+console.log(parseInt('17',8));
+
+console.log(!0);
+console.log(!!0);
+console.log(!!NaN);
+console.log(Boolean(NaN));
+console.log(Boolean({}));
