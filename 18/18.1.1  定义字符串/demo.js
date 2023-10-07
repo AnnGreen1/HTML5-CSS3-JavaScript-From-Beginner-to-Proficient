6bade9dde1c8745ffb56ad7ea685d6139fb30c64
anngreen <anngreen@qq.com>
2023-10-07 22:46:15 +0800

    15、16、17、18的部分

---
+++ 18/18.1.1  定义字符串/demo.js
@@ -0,0 +1,3 @@
+var s = String(1,2,3,4,5,6)
+console.log(s);
+console.log(typeof s);
---
+++ 18/18.1.5  字符串截取/demo.js
@@ -0,0 +1,9 @@
+let str = "Hello, World!";
+console.log(str.substring(12, 7));
+console.log(str.substring('s', 7));
+console.log(str.substring(7, 's'));
+
+
+console.log(str.slice(12, 7));
+console.log(str.slice('s', 7));
+console.log(str.slice(7, 's'));
